20fec1d26479062efac3d036727aab0bbcd15de7
Stefan Richter <stefan.richter3@student.hpi.uni-potsdam.de>
2014-01-29 16:10:38 +0100

    AppLockService uses RunningTaskInfoResolver

--- AppLockMain/src/io/github/sealor/android/applock/AppLockService.java
+++ AppLockMain/src/io/github/sealor/android/applock/AppLockService.java
@@ -1,29 +1,27 @@
 package io.github.sealor.android.applock;
 
-import java.util.List;
 import java.util.Set;
 
-import android.app.ActivityManager;
-import android.app.ActivityManager.RunningTaskInfo;
 import android.app.Service;
-import android.content.Context;
 import android.content.Intent;
 import android.os.IBinder;
 
 public class AppLockService extends Service {
 
+	private RunningTaskInfoResolver runningTaskInfoResolver;
+
 	private Set<String> restrictedPackageNames;
+
+	@Override
+	public void onCreate() {
+		super.onCreate();
+
+		this.runningTaskInfoResolver = new RunningTaskInfoResolver(this);
+	}
 
 	@Override
 	public IBinder onBind(Intent intent) {
 		return null;
-	}
-
-	public String resolveRunningAppPackage() {
-		final ActivityManager activityManager = (ActivityManager) getSystemService(Context.ACTIVITY_SERVICE);
-		final List<RunningTaskInfo> services = activityManager.getRunningTasks(1);
-		RunningTaskInfo runningApp = services.get(0);
-		return runningApp.topActivity.getPackageName();
 	}
 
 	public void setRestrictedPackageNames(Set<String> restrictedPackageNames) {
@@ -31,6 +29,6 @@ public class AppLockService extends Service {
 	}
 
 	public boolean isRunningAppRestricted() {
-		return this.restrictedPackageNames.contains(resolveRunningAppPackage());
+		return this.restrictedPackageNames.contains(this.runningTaskInfoResolver.resolveRunningAppPackageName());
 	}
 }
