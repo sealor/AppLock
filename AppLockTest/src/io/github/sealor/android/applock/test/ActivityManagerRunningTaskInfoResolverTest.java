3f21f7308c00c588956826dc1ed1f8a67cebf264
Stefan Richter <stefan.richter3@student.hpi.uni-potsdam.de>
2014-01-30 12:23:26 +0100

    ActivityManagerRunningTaskInfoResolverTest extends AndroidTestCase instead of ServiceTestCase

--- AppLockTest/src/io/github/sealor/android/applock/test/ActivityManagerRunningTaskInfoResolverTest.java
+++ AppLockTest/src/io/github/sealor/android/applock/test/ActivityManagerRunningTaskInfoResolverTest.java
@@ -2,24 +2,14 @@ package io.github.sealor.android.applock.test;
 
 import static io.github.sealor.android.applock.test.tooling.TestUtils.resolveOwnPackageName;
 import static io.github.sealor.android.applock.test.tooling.TestUtils.startTestActivity;
+import io.github.sealor.android.applock.taskinfo.ActivityManagerRunningTaskInfoResolver;
 import io.github.sealor.android.applock.taskinfo.RunningTaskInfoResolver;
-import android.content.Intent;
-import android.test.ServiceTestCase;
+import android.test.AndroidTestCase;
 
-public class ActivityManagerRunningTaskInfoResolverTest extends ServiceTestCase<TestFactoryService> {
-
-	public ActivityManagerRunningTaskInfoResolverTest() {
-		super(TestFactoryService.class);
-	}
-
-	@Override
-	protected void setUp() throws Exception {
-		super.setUp();
-		startService(new Intent());
-	}
+public class ActivityManagerRunningTaskInfoResolverTest extends AndroidTestCase {
 
 	public void testResolveRunningAppPackageName() {
-		RunningTaskInfoResolver resolver = getService().createActivityManagerRunningTaskInfoResolver();
+		RunningTaskInfoResolver resolver = new ActivityManagerRunningTaskInfoResolver(getContext());
 		startTestActivity(getContext());
 
 		String runningAppPackageName = resolver.resolveRunningAppPackageName();
