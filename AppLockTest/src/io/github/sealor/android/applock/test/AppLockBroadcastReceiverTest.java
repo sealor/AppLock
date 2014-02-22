b9ba6d811e5415427b6044344061ca39e60703a1
Stefan Richter <jdevsr3@googlemail.com>
2014-02-22 11:21:13 +0100

    test for BOOT_COMPLETED_BROADCAST added

--- AppLockTest/src/io/github/sealor/android/applock/test/AppLockBroadcastReceiverTest.java
+++ AppLockTest/src/io/github/sealor/android/applock/test/AppLockBroadcastReceiverTest.java
@@ -1,6 +1,7 @@
 package io.github.sealor.android.applock.test;
 
 import io.github.sealor.android.applock.AppLockBroadcastReceiver;
+import android.content.ComponentName;
 import android.content.Intent;
 import android.os.Bundle;
 import android.test.AndroidTestCase;
@@ -17,6 +18,15 @@ public class AppLockBroadcastReceiverTest extends AndroidTestCase {
 		assertTrue(myMockContext.isActivityStarted);
 	}
 
+	public void testStartAppLockServiceOnBootCompletedBroadcast() throws InterruptedException {
+		MyMockContext myMockContext = new MyMockContext();
+
+		AppLockBroadcastReceiver receiver = new AppLockBroadcastReceiver();
+		receiver.onReceive(myMockContext, new Intent(Intent.ACTION_BOOT_COMPLETED));
+
+		assertTrue(myMockContext.isServiceStarted);
+	}
+
 	public void testDoNothingOnAnyBroadcast() throws InterruptedException {
 		MyMockContext myMockContext = new MyMockContext();
 
@@ -29,6 +39,7 @@ public class AppLockBroadcastReceiverTest extends AndroidTestCase {
 	private class MyMockContext extends MockContext {
 
 		private boolean isActivityStarted = false;
+		private boolean isServiceStarted = false;
 
 		@Override
 		public String getPackageName() {
@@ -43,6 +54,12 @@ public class AppLockBroadcastReceiverTest extends AndroidTestCase {
 		@Override
 		public void startActivity(Intent intent, Bundle options) {
 			this.isActivityStarted = true;
+		}
+
+		@Override
+		public ComponentName startService(Intent intent) {
+			this.isServiceStarted = true;
+			return null;
 		}
 	}
 }
